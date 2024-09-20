ae8daf8e7ce21f1d3bbd876f5e988f2118186c90
Petrik Diak <diak@petrik.hu>
2024-09-20 16:35:50 +0200

    quotes, getrandomQuotes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { quotes } from './quotes (1)';
 
 @Controller()
 export class AppController {
@@ -12,4 +13,31 @@ export class AppController {
       message: this.appService.getHello()
     };
   }
+
+@Get('/quotes')
+@Render('quotes')
+getQuotes(){
+  return {quotes: quotes};
+}
+
+@Get('/randomQuote')
+@Render('randomQuote')
+getrandomQuote(){
+  const min = 1;
+  const max = quotes.quotes.length;
+  const num = Math.floor(Math.random()* max)
+
+  const randomQuote = quotes.quotes[num]
+  
+  return {
+    quote: randomQuote.quote,
+    author: randomQuote.author
+  }
+}
+
+@Get('/topAuthors')
+@Render('topAuthors')
+gettopAuthors(){
+  
+}
 }
